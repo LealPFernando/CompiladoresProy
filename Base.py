ad02e86fcc75a73f4763eebb404a6f394e36a88a
Fernando Leal <f.leal@live.com.mx>
2020-10-05 00:32:00 -0600

    Funciones incompletas

--- Base.py
+++ Base.py
@@ -3,7 +3,7 @@ from sly import Parser
 
 class Duck2020Lexer(Lexer):
     tokens = { PROGRAM, ID, VAR, INT, FLOAT, ASSIGN, GT, LT, NT, IF, ELSE, PLUS, MINUS, TIMES, DIVIDE, CTEINT, CTEFLOAT, \
-    CTESTRING, PRINT, MAIN, CHAR, EQUAL, AND, OR, LINE, POINT, CIRCLE, ARC, PENUP, PENDOWN, COLOR, SIZE, CLEAR, DO, WHILE, \
+    CTESTRING, CTECHAR, PRINT, MAIN, CHAR, EQUAL, AND, OR, LINE, POINT, CIRCLE, ARC, PENUP, PENDOWN, COLOR, SIZE, CLEAR, DO, WHILE, \
     FOR, TO}
 
     literals = { ';', ':', '(', ')', '{', '}',',' }
@@ -46,6 +46,7 @@ class Duck2020Lexer(Lexer):
     DIVIDE  = r'/'
     CTEFLOAT  = r'\d+\.\d+'
     CTEINT  = r'\d+'
+    CTECHAR = r'[a-zA-Z_]'
     CTESTRING = r'\".*?\"'
 
 
@@ -55,90 +56,69 @@ class Duck2020Parser(Parser):
     
     tokens = Duck2020Lexer.tokens
 
-    @_('PROGRAM ID ";" vars bloque', 'PROGRAM ID ";" bloque')
-    def programa(self, p):
+    #Decision
+    @_('if "(" expresion ")" "{" estatuto decision2 "}"', 'if "(" expresion ")" "{" estatuto decision2 "}" else "{" estatuto decision2 "}"')
+    def decision(self, p):
         pass
 
-    @_('VAR vars2')
-    def vars(self, p):
+    @_('estatuto decision2', '')
+    def decision2(self, p):
         pass
 
-    @_('ID vars3 ":" tipo ";" vars4')
-    def vars2(self, p):
+    #FuncionesESP
+    @_('line "(" exp "," exp ")"', 'point "(" exp "," exp ")"', 'circle "(" exp ")"', 'arc "(" exp "," exp "," exp ")"', 'penup "(" ")"', 'pendown "(" ")"', 'color "(" exp ")"', 'size "(" exp ")"', 'clear "(" ")"')
+    def funcionesesp(self, p):
         pass
 
-    @_('"," ID vars3', '"," ID', '')
-    def vars3(self, p):
-        pass
-
-    @_('vars2', '')
-    def vars4(self, p):
-        pass
-
-    @_('INT', 'FLOAT')
-    def tipo(self, p):
-        pass
-
-    @_('"{" estatuto bloque2 "}"', '"{" "}"')
-    def bloque(self, p):
-        pass
-
-    @_('estatuto bloque2','estatuto', '')
-    def bloque2(self, p):
-        pass
-
-    @_('asignacion', 'condicion', 'escritura')
-    def estatuto(self, p):
-        pass
-
-    @_('ID ASSIGN expresion ";"')
-    def asignacion(self, p):
-        pass
-
-    @_('PRINT "(" escritura2 escritura3 ")" ";"')
-    def escritura(self, p):
-        pass
-
-    @_('expresion', 'CTESTRING')
-    def escritura2(self, p):
-        pass
-
-    @_('"," escritura2 escritura3', '"," escritura2', '')
-    def escritura3(self, p):
-        pass
-
-    @_('IF "(" expresion ")" bloque ELSE bloque ";"', 'IF "(" expresion ")" bloque ";"')
-    def condicion(self, p):
-        pass
-
-    @_('exp GT exp', 'exp LT exp', 'exp NT exp', 'exp')
+    #Expresion
+    @_('exp', 'exp GT exp', 'exp LT exp', 'exp NT exp', 'exp EQUAL exp', 'exp AND exp', 'exp OR exp')
     def expresion(self, p):
         pass
 
+    #EXP
     @_('termino exp2')
     def exp(self, p):
         pass
 
-    @_('PLUS exp', 'MINUS exp', '')
+    @_('PLUS termino', 'MINUS termino', '')
     def exp2(self, p):
         pass
 
+    #Termino
     @_('factor termino2')
     def termino(self, p):
         pass
 
-    @_('TIMES factor', 'DIVIDE factor', '')
+    @_('TIMES factor', "DIVIDE factor", '')
     def termino2(self, p):
         pass
 
-    @_('"(" expresion ")"','PLUS varcte', 'MINUS varcte', 'varcte', '')
+    #FACTOR
+    @_('"(" expresion ")"', 'factor2 varcte')
     def factor(self, p):
         pass
 
-    @_('ID', 'CTEINT', 'CTEFLOAT')
+    @_('PLUS', 'MINUS', '')
+    def factor2(self, p):
+        pass
+
+
+    #VARCTE 
+    #El problema de VARCTE es que no se pueden llamar funciones sin parametros se tendria que modificar la regla para que accepte
+    #Tambien verificar que el token declarado de char sea el correcto, ya que no se puede verficar por el ID
+    @_('CTEINT', 'CTEFLOAT', 'CTECHAR', 'ID varcte2')
     def varcte(self, p):
         pass
 
+    @_('"(" ID varcte3 ")"', '')
+    def varcte2(self, p):
+        pass
+
+    @_('"," ID varcte3', '')
+    def varcte3(self, p):
+        pass
+
+    
 
 # if __name__ == '__main__':
 #     lexer = Duck2020Lexer()
@@ -156,26 +136,54 @@ class Duck2020Parser(Parser):
 
 if __name__ == '__main__':
 
-    file = open("test.txt", 'r')
-    masterline = ""
+    # file = open("test.txt", 'r')
+    # masterline = ""
 
-    for line in file:
-        masterline = masterline + line.strip()
+    # for line in file:
+    #     masterline = masterline + line.strip()
 
     lexer = Duck2020Lexer()
     parser = Duck2020Parser()
-    result = parser.parse(lexer.tokenize(masterline))
-    print(result)
-    # while True:
-    #     try:
-    #         text = input('duck > ')
-    #         result = parser.parse(lexer.tokenize(text))
-    #         print(result)
-    #         # for line in file:
-    #         #     result = parser.parse(lexer.tokenize(line.strip()))
-    #         #     print(result)
+    # result = parser.parse(lexer.tokenize(masterline))
+    # print(result)
+    while True:
+        try:
+            text = input('duck > ')
+            result = parser.parse(lexer.tokenize(text))
+            print(result)
+            # for line in file:
+            #     result = parser.parse(lexer.tokenize(line.strip()))
+            #     print(result)
 
-    #     except EOFError:
-    #         break
+        except EOFError:
+            break
     
-    file.close()
+    # file.close()
+
+
+#-----------Main original
+    # if __name__ == '__main__':
+
+    # file = open("test.txt", 'r')
+    # masterline = ""
+
+    # for line in file:
+    #     masterline = masterline + line.strip()
+
+    # lexer = Duck2020Lexer()
+    # parser = Duck2020Parser()
+    # result = parser.parse(lexer.tokenize(masterline))
+    # print(result)
+    # # while True:
+    # #     try:
+    # #         text = input('duck > ')
+    # #         result = parser.parse(lexer.tokenize(text))
+    # #         print(result)
+    # #         # for line in file:
+    # #         #     result = parser.parse(lexer.tokenize(line.strip()))
+    # #         #     print(result)
+
+    # #     except EOFError:
+    # #         break
+    
+    # file.close()
